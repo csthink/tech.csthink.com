1addc0f1f0fa08369b53a764b564c5142524d260
csthink <security.2009@live.cn>
2019-08-10 21:37:01 +0800

    修复中文侧边栏配置错误

--- docs/.vuepress/config/sidebar/zh.js
+++ docs/.vuepress/config/sidebar/zh.js
@@ -1,12 +1,33 @@
 const java = [
   {
     title: '基础篇',
-    collapsable: false,
+    // collapsable: false,
     children: [
       '/zh/Java/Java基础',
-      '/zh/Java/Java类集框架'
+      '/zh/Java/类集框架',
+      '/zh/Java/异常机制',
+      '/zh/Java/Servlet 与 JSP',
+      '/zh/Java/JDBC 入门'
     ]
   },
+  {
+    title: '进阶篇',
+    // collapsable: false,
+    children: [
+      '/zh/Java/Spring 简介以及 Bean 管理',
+      '/zh/Java/Spring AOP 入门篇',
+      '/zh/Java/Spring 事务管理',
+      '/zh/Java/Spring 基于 AspectJ 的 AOP 开发'
+    ]
+  },
+  {
+    title: '高级篇',
+    // collapsable: false,
+    children: [
+      '/zh/Java/软件设计原则-上',
+    ]
+  },
+
 ]
 
 module.exports = {
